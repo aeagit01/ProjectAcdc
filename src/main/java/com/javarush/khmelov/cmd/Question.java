455b24ab24924ab31fef8f3d8b8286ac31a8feca
Evgeny Alpeyev <krotpk@yandex.ru>
2024-04-08 21:36:29 +0300

    refactoring Question.java

--- src/main/java/com/javarush/khmelov/cmd/Question.java
+++ src/main/java/com/javarush/khmelov/cmd/Question.java
@@ -1,45 +1,34 @@
 package com.javarush.khmelov.cmd;
 
-import com.javarush.khmelov.entity.FinishMessage;
+
 import com.javarush.khmelov.entity.QuestObject;
 import com.javarush.khmelov.service.QuestResponsesService;
-import com.javarush.khmelov.service.QuestService;
 import com.javarush.khmelov.service.QuestionService;
-import jakarta.servlet.ServletException;
+import com.javarush.khmelov.tools.Keys;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Optional;
+
 
 public class Question implements Command{
-    private final QuestService questService;
     private final QuestionService questionService;
     private final QuestResponsesService questResponsesService;
 
-    public Question(QuestService questService, QuestionService questionService, QuestResponsesService questResponsesService) {
-        this.questService = questService;
+    public Question(QuestionService questionService, QuestResponsesService questResponsesService) {
         this.questionService = questionService;
         this.questResponsesService = questResponsesService;
     }
 
-
     @Override
     public String doPost(HttpServletRequest req, HttpServletResponse res) {
-        String selection = req.getParameter("responselist");
-        String returnString = "";
+        String selection = req.getParameter(Keys.JSP_VAL_RESPONSELIST); //"responselist"
 
-        return getPage() + "?question=" + Long.parseLong(selection) ;
+        return getPage() + "?%s=%s".formatted(Keys.QUESTION,selection); //question
     }
 
     public String doGet(HttpServletRequest req, HttpServletResponse res) {
-        ArrayList<QuestObject> responses = new ArrayList<>() ;
 
-        String stringId = req.getParameter("id");
-        String returnPage = getJspPage();
-
-
-        return returnPage;
+        return getJspPage();
     }
 }
